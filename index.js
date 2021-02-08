2acf09dd924132b35d47190d64c9b037dd2f19b0
VictorNAGomes <victor.gomes63@etec.sp.gov.br>
2021-02-07 22:45:55 -0300

    Dividindo as rotas em diferentes arquivos.

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const connection = require("./database/database");
 
+const categoriesController = require("./categories/CategoriesController");
+const articlesController = require("./articles/ArticlesController");
 
 //view engine
 app.set('view engine', 'ejs');
@@ -12,6 +15,18 @@ app.use(express.static('public'));
 //body parser
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+
+//connection
+connection
+    .authenticate()
+    .then(()=>{
+        console.log("Conexão feita com sucesso!")
+    }).catch((err)=>{
+        console.log("ERRO: " + err);
+    });
+
+app.use("/", categoriesController);
+app.use("/", articlesController);
 
 app.get("/", (req, res)=>{
     res.render("index");
