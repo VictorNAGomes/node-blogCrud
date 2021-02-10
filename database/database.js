0f70761e17459473e90bfa57affe7f7e784eeca7
VictorNAGomes <victor.gomes63@etec.sp.gov.br>
2021-02-10 11:25:54 -0300

    Mudando fuso horario do Sequelize

--- database/database.js
+++ database/database.js
@@ -2,7 +2,8 @@ const Sequelize = require("sequelize");
 
 const connection = new Sequelize('blogCrud', 'root', '123',{
 host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    timezone:"-03:00"
 });
 
 module.exports = connection;
