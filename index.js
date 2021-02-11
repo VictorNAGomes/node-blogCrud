fa91cfcce7e8687b2d4b7d69f77cc702a005c777
VictorNAGomes <victor.gomes63@etec.sp.gov.br>
2021-02-11 18:14:47 -0300

    Criando rota para exibiçao de artigo

--- index.js
+++ index.js
@@ -40,6 +40,23 @@ app.get("/", (req, res)=>{
     });
 });
 
+app.get("/:slug", (req, res)=>{
+    var slug = req.params.slug;
+    Article.findOne({
+        where: {
+            slug: slug
+        }
+    }).then(article => {
+        if(article != undefined){
+            res.render("article", {article:article});
+        }else{
+            res.redirect("/");
+        }
+    }).catch(err => {
+        res.redirect("/");
+    });
+});
+
 app.listen(3001, ()=>{
     console.log("O servidor está rodando");
 })
