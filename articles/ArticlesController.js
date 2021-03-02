70fef77cbc76b7ac09971e688fe2c0302849de7c
VictorNAGomes <victor.gomes63@etec.sp.gov.br>
2021-03-02 12:25:44 -0300

    Finalizando a paginaçao

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -127,6 +127,7 @@ router.get("/articles/page/:num", (req, res) => {
         }
 
         var result = {
+            page: parseInt(page),
             next: next,
             articles: articles
         }
