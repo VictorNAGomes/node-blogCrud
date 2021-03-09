8d131196d85e37989c40c9f9ac23b05c1ab10a52
VictorNAGomes <victor.gomes63@etec.sp.gov.br>
2021-03-08 22:31:25 -0300

    Evitando a criaçao de usuarios com emails existentes

--- users/UsersController.js
+++ users/UsersController.js
@@ -4,7 +4,7 @@ const User = require('./User');
 const bcrypt = require('bcryptjs');
 
 
-router.get("admin/users", (req, res)=>{
+router.get("/admin/users", (req, res)=>{
     res.send("Listagem de usuários");
 });
 
@@ -16,16 +16,25 @@ router.post("/users/create",(req, res)=>{
     var email = req.body.email;
     var password = req.body.password;
 
-    var salt = bcrypt.genSaltSync(10);
-    var hash = bcrypt.hashSync(password, salt);
-    
-    User.create({
-        email: email,
-        password: hash
-    }).then((res, req)=>{
-        res.redirect("/");
-    }).catch(err=>{
-        res.redirect("/");
+    User.findOne({where:{
+        email:email
+    }}).then(user =>{
+        if (user == undefined){
+
+            var salt = bcrypt.genSaltSync(10);
+            var hash = bcrypt.hashSync(password, salt);
+            
+            User.create({
+                email: email,
+                password: hash
+            }).then((res, req)=>{
+                res.redirect("/");
+            }).catch(err=>{
+                res.redirect("/");
+    });
+        }else{
+            res.redirect("/admin/users/create");
+        }
     });
 });
 
